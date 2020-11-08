a3d70e0e4fb5a31d9d79225cdb471d6fba02a48f
Shelcia abi <shelciaabi@Shelcias-Air.bbrouter>
2020-11-08 23:07:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./components/mainpage/HomePage";
+import "./styles/style.css";
 
 const App = () => {
   return (
--- src/components/mainpage/SideNav.jsx
+++ src/components/mainpage/SideNav.jsx
@@ -1,14 +1,61 @@
-import React from 'react';
+import React from "react";
 
 const SideNav = () => {
-    return ( <React.Fragment>
-        <ul>
-            <li>jkjkj</li>
-            <li>jkjkj</li>
-            <li>jkjkj</li>
-            <li>jkjkj</li>
-        </ul>
-    </React.Fragment> );
-}
- 
+  return (
+    <React.Fragment>
+      <div
+        className="nav flex-column nav-pills border-right"
+        id="v-pills-tab"
+        role="tablist"
+        aria-orientation="vertical"
+      >
+        <a
+          className="nav-link active"
+          id="v-pills-home-tab"
+          data-toggle="pill"
+          href="#v-pills-home"
+          role="tab"
+          aria-controls="v-pills-home"
+          aria-selected="true"
+        >
+          Home
+        </a>
+        <a
+          className="nav-link"
+          id="v-pills-profile-tab"
+          data-toggle="pill"
+          href="#v-pills-profile"
+          role="tab"
+          aria-controls="v-pills-profile"
+          aria-selected="false"
+        >
+          Profile
+        </a>
+        <a
+          className="nav-link"
+          id="v-pills-messages-tab"
+          data-toggle="pill"
+          href="#v-pills-messages"
+          role="tab"
+          aria-controls="v-pills-messages"
+          aria-selected="false"
+        >
+          Messages
+        </a>
+        <a
+          className="nav-link"
+          id="v-pills-settings-tab"
+          data-toggle="pill"
+          href="#v-pills-settings"
+          role="tab"
+          aria-controls="v-pills-settings"
+          aria-selected="false"
+        >
+          Settings
+        </a>
+      </div>
+    </React.Fragment>
+  );
+};
+
 export default SideNav;
