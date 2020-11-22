85c5cd1c1810901a802c4a426c6a25642f84b393
Shelcia abi <msm17b011@iiitdm.ac.in>
2020-11-22 23:54:35 +0530

    update

---
+++ src/components/adminpage/Login.jsx
@@ -0,0 +1,107 @@
+import React, { useRef, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import Navbar from "../LandingPage/Navbar";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Login = () => {
+  const email = useRef("");
+  const password = useRef("");
+  const LINK = process.env.REACT_APP_ADMINAUTH_API;
+  const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
+  const PREFIX = "NoobCode-";
+
+  const sucessNotify = (message) => {
+    toast.success(message);
+  };
+  const failedNotify = (message) => {
+    toast.error(message);
+  };
+
+  const onSubmit = (event) => {
+    setIsLoading(true);
+    localStorage.setItem(`${PREFIX}Email`, email.current.value);
+    event.preventDefault();
+    axios
+      .post(`${LINK}signin`, {
+        email: email.current.value,
+        password: password.current.value,
+      })
+      .then((res) => {
+        // console.log(res);
+        setIsLoading(false);
+        localStorage.setItem(`${PREFIX}Token`, res.data.token);
+        localStorage.setItem(`${PREFIX}UserId`, res.data.userId);
+        localStorage.setItem(`${PREFIX}name`, res.data.name);
+        sucessNotify("Login succesfulll");
+        history.push("/dashboard/feed");
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        failedNotify("Incorrect Credentials");
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <ToastContainer />
+      <Navbar />
+      <div className="container" id="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <h3 className="text-center">Login</h3>
+            <form className="was-validated" onSubmit={onSubmit}>
+              <div className="form-group">
+                <label htmlFor="uname">Email:</label>
+                <input
+                  type="email"
+                  ref={email}
+                  className="form-control"
+                  id="uname"
+                  placeholder="Enter email"
+                  name="uname"
+                  required
+                />
+                <div className="invalid-feedback">
+                  Please enter valid email.
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="pwd">Password:</label>
+                <input
+                  type="password"
+                  ref={password}
+                  className="form-control"
+                  id="pwd"
+                  placeholder="Enter password"
+                  name="pswd"
+                  required
+                />
+                <div className="invalid-feedback">
+                  Please fill out this field.
+                </div>
+              </div>
+              <div className="text-center">
+                {isLoading ? (
+                  <p>We are verifying...</p>
+                ) : (
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                )}
+              </div>
+            </form>
+            <div className="text-center mt-5">
+              Don't have an account? then <Link to="/signup">Signup</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Login;
