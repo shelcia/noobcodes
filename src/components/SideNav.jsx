493ea92f4b2090aebc3da88b24660d480750a43c
Shelcia abi <msm17b011@iiitdm.ac.in>
2020-11-23 03:15:15 +0530

    update sidenav to be more dynamic

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { CodeContext } from "./context/CodeContext";
 import { ThemeContext } from "./context/ThemeContext";
+import SideNavComp from "./SideNavComp";
 
 const SideNav = () => {
   const [darkTheme] = useContext(ThemeContext);
@@ -23,64 +24,13 @@ const SideNav = () => {
         <a href="#home" className="nav-link" data-toggle="collapse">
           Home
         </a>
-
         <div id="home" className="collapse">
           <NavLink className="nav-link pl-5" strict to="/">
             Home
           </NavLink>
         </div>
-
-        <a href="#array" className="nav-link" data-toggle="collapse">
-          Arrays
-        </a>
-
-        <div id="array" className="collapse">
-          <ul className="list-group list-group-flush">
-            {arrayCodes.map((code) => (
-              <li
-                className={
-                  darkTheme
-                    ? "list-group-item bg-dark p-0"
-                    : "list-group-item bg-light p-0"
-                }
-              >
-                <NavLink
-                  className="nav-link pl-5"
-                  exact
-                  to={`/code/${code.codeId}`}
-                >
-                  {code.title}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <a href="#linkedlist" className="nav-link" data-toggle="collapse">
-          Linked List
-        </a>
-
-        <div id="linkedlist" className="collapse">
-          <ul className="list-group list-group-flush">
-            {llCodes.map((code) => (
-              <li
-                className={
-                  darkTheme
-                    ? "list-group-item bg-dark p-0"
-                    : "list-group-item bg-light p-0"
-                }
-              >
-                <NavLink
-                  className="nav-link pl-5"
-                  exact
-                  to={`/code/${code.codeId}`}
-                >
-                  {code.title}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <SideNavComp title="Arrays" codes={arrayCodes} />
+        <SideNavComp title="Linked Lists" codes={llCodes} />
       </div>
     </React.Fragment>
   );
---
+++ src/components/SideNavComp.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
+import { NavLink } from "react-router-dom";
+
+const SideNavComp = ({ codes, title }) => {
+  const [darkTheme] = useContext(ThemeContext);
+  return (
+    <React.Fragment>
+      <a
+        href={`#${title.replace(/\s+/g, "").toLowerCase()}`}
+        className="nav-link"
+        data-toggle="collapse"
+      >
+        {title}
+      </a>
+      <div
+        id={`${title.replace(/\s+/g, "").toLowerCase()}`}
+        className="collapse"
+      >
+        <ul className="list-group list-group-flush">
+          {codes.map((code) => (
+            <li
+              className={
+                darkTheme
+                  ? "list-group-item bg-dark p-0"
+                  : "list-group-item bg-light p-0"
+              }
+            >
+              <NavLink
+                className="nav-link pl-5"
+                exact
+                to={`/code/${code.codeId}`}
+              >
+                {code.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default SideNavComp;
